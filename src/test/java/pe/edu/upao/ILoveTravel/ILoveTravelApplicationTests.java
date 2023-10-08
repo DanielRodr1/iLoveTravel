014a689cdc8fa0f198ade1dad8e6730cfbf577fb
unknown <drodrigueza10@upao.edu.pe>
2023-10-07 23:08:38 -0500

    first commit

---
+++ src/test/java/pe/edu/upao/ILoveTravel/ILoveTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.ILoveTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILoveTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
